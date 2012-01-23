34cd17db45a5c6422d76d28a0c6568b507804722
Tommy Bishop <bishop.thomas@gmail.com>
2012-01-23 14:22:29 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theoverheards::Application.config.secret_token = '97030e84570f88a08ce24f377f3b7e0fd40d4195c3122ff1c287925f3a0fefad110d37c464cbf3a1007cfb0791150602fd2717d3189385ba6bdcdc62fc773fc1'
